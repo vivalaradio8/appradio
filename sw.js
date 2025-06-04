4f69ad301a3a371572979409fbab92084134427b
Fernando Medina <vivalaradio80@gmail.com>
2025-06-04 11:25:31 -0300

    Agrego service worker y registro en index.html

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activado');
+});
